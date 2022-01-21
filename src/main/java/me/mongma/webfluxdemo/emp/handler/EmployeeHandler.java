7f3094fdad17d6c0a2c0136c32736b375c3ef355
imac <shnam0110@naver.com>
2022-01-21 14:16:34 +0900

    Add Postgresql Database, CRUD sample completed.

--- src/main/java/me/mongma/webfluxdemo/emp/handler/EmployeeHandler.java
+++ src/main/java/me/mongma/webfluxdemo/emp/handler/EmployeeHandler.java
@@ -12,8 +12,7 @@ import reactor.core.publisher.Mono;
 
 import java.net.URI;
 
-import static org.springframework.web.reactive.function.server.ServerResponse.created;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import static org.springframework.web.reactive.function.server.ServerResponse.*;
 
 @Component
 public class EmployeeHandler {
@@ -35,23 +34,38 @@ public class EmployeeHandler {
 
     public Mono<ServerResponse> getAll(ServerRequest req) {
         Flux<Employee> employees = repo.findAll();
-        return ok().contentType(MediaType.APPLICATION_JSON).body(employees, Employee.class)
+        return ok().contentType(MediaType.APPLICATION_JSON)
+                .body(employees, Employee.class)
                 .switchIfEmpty(Mono.empty());
     }
 
     public Mono<ServerResponse> addEmployee(ServerRequest req) {
         Mono<Employee> employee = req.bodyToMono(Employee.class);
-        return repo.saveAll(employee)
-                .flatMap(emp -> created(URI.create("/v1/employee/")).build())
-                .next();
+        return employee
+                .flatMap(emp -> repo.save(emp))
+                .flatMap(emp -> created(URI.create("/v1/employee/" + emp.getSeq())).build());
     }
 
     public Mono<ServerResponse> modifyEmployee(ServerRequest req) {
-        return null;
+        return Mono.zip(
+                    (data) -> {
+                        Employee emp1 = (Employee) data[0];
+                        Employee emp2 = (Employee) data[1];
+                        emp1.setName(emp2.getName());
+                        emp1.setSalary(emp2.getSalary());
+                        emp1.setDepartment(emp2.getDepartment());
+                        return emp1;
+                    },
+                    repo.findById(Long.valueOf(req.pathVariable("id"))),
+                    req.bodyToMono(Employee.class)
+                )
+                .cast(Employee.class)
+                .flatMap(emp -> repo.save(emp))
+                .flatMap(emp -> created(URI.create("/v1/employee/" + emp.getSeq())).build());
     }
 
     public Mono<ServerResponse> fireEmployee(ServerRequest req) {
-        return ok().body(repo.deleteById(Long.valueOf(req.pathVariable("id"))), Employee.class);
+        return ok().body((repo.deleteById(Long.valueOf(req.pathVariable("id")))), Employee.class);
     }
 
 }
