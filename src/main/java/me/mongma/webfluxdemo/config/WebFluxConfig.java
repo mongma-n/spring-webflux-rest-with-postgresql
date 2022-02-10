7aed96e943c6395b05355b158b2dfa60df1687a6
mongma-n <mongma-n@naver.com>
2022-02-10 16:44:11 +0900

    CORS Webfilter 추가

--- src/main/java/me/mongma/webfluxdemo/config/WebFluxConfig.java
+++ src/main/java/me/mongma/webfluxdemo/config/WebFluxConfig.java
@@ -1,11 +1,19 @@
 package me.mongma.webfluxdemo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 @EnableWebFlux
-public class WebFluxConfig implements WebFluxConfigurer
-{  
+public class WebFluxConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        registrar.setUseIsoFormat(true);
+        registrar.registerFormatters(registry);
+    }
 }
--- src/main/java/me/mongma/webfluxdemo/emp/router/EmployeeRouter.java
+++ src/main/java/me/mongma/webfluxdemo/emp/router/EmployeeRouter.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -18,7 +17,7 @@ public class EmployeeRouter {
     private final EmployeeHandler handler;
 
     @Bean
-    public RouterFunction<ServerResponse> routes() {
+    public RouterFunction<?> routes() {
         return route()
                 .path("/apis/v1/employee", builder -> builder
                     .nest(accept(MediaType.APPLICATION_JSON), builder1 -> builder1
